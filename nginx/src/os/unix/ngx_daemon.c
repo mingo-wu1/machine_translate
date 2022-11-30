f9c9ae79d041ab3fba4a15f922f168a9e1f4f5dd
mingo-wu <57790355+mingo-wu1@users.noreply.github.com>
2022-12-01 02:26:21 +0800

    Update ngx_daemon.c

--- nginx/src/os/unix/ngx_daemon.c
+++ nginx/src/os/unix/ngx_daemon.c
@@ -14,41 +14,41 @@ ngx_daemon(ngx_log_t *log)
 {
     int  fd;
 
-    switch (fork()) {
-    case -1:
+    switch (fork()) { // 用fork创建守护进程, 如果fork成功，子进程中fork的返回值是0，父进程中fork的返回值是子进程的进程号，如果fork不成功，父进程会返回错误
+    case -1: // fork返回-1创建失败
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "fork() failed");
         return NGX_ERROR;
 
-    case 0:
+    case 0: // 子进程返回 ,子进程从fork返回处开始执行，操作系统会复制一个与父进程完全相同的子进程，虽说是父子关系，但是在操作系统看来，他们更像兄弟关系，这2个进程共享代码空间，但是数据空间是互相独立的，子进程数据空间中的内容是父进程的完整拷贝，指令指针也完全相同，子进程拥有父进程当前运行到的位置（两进程的程序计数器pc值相同，也就是说，子进程是从fork返回处开始执行的）
         break;
 
-    default:
-        exit(0);
+    default: //父进程返回
+        exit(0); //父进程退出
     }
 
     ngx_parent = ngx_pid;
     ngx_pid = ngx_getpid();
 
-    if (setsid() == -1) {
+    if (setsid() == -1) { // 建立新的会话，然后子进程称为会话组长
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "setsid() failed");
         return NGX_ERROR;
     }
 
-    umask(0);
+    umask(0); // 重设文件创建掩模
 
-    fd = open("/dev/null", O_RDWR);
+    fd = open("/dev/null", O_RDWR); /*重定向标准输入、输出到/dev/null(传说中的黑洞)*/
     if (fd == -1) {
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno,
                       "open(\"/dev/null\") failed");
         return NGX_ERROR;
     }
 
-    if (dup2(fd, STDIN_FILENO) == -1) {
+    if (dup2(fd, STDIN_FILENO) == -1) { // 输入重定向到fd，即从/dev/null输入
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "dup2(STDIN) failed");
         return NGX_ERROR;
     }
 
-    if (dup2(fd, STDOUT_FILENO) == -1) {
+    if (dup2(fd, STDOUT_FILENO) == -1) { // 输出重定向到fd，即所有输出到/dev/null
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "dup2(STDOUT) failed");
         return NGX_ERROR;
     }
